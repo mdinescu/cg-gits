d6d2842439bb56770d2a66a43e50e5d3c4ffe13c
Mike Dinescu <mdinescu@donaq.com>
2017-02-25 16:47:26 -0800

    wood 1 - with multiple commands

--- gits.cs
+++ gits.cs
@@ -5,13 +5,10 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player
 {
     public static int ME = 1;
+    public static int CPU = -1;
     public static string FACTORY = "FACTORY";
     
     
@@ -51,7 +48,7 @@ class Player
     }
     
     static void ComputeShortestPaths(Dictionary<int, Cell> cells) {
-        int N = cells.Count;
+        int N = cells.Count; int MXX = int.MaxValue;
         SP_D = new int[N][]; NEXT = new int[N][];
         for(int i = 0; i < N; i++) {
             SP_D[i] = new int[N]; NEXT[i] = new int[N];
@@ -60,11 +57,11 @@ class Player
                     SP_D[i][j] = cells[i].Links[j].Dist;
                     NEXT[i][j] = j;
                 }else {
-                    SP_D[i][j] = 999;    
+                    SP_D[i][j] = MXX;    
                     NEXT[i][j] = -1;
                 }                
                 
-                Console.Error.Write("{0:D3} ", NEXT[i][j]);
+                //Console.Error.Write("{0:D3} ", NEXT[i][j]);
             }
             Console.Error.WriteLine();
         }
@@ -72,10 +69,10 @@ class Player
         for(int k = 0; k < N; k++) {
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < N; j++) {
-                    int d = (SP_D[i][k] != 999 && 
-                             SP_D[k][j] != 999)
+                    int d = (SP_D[i][k] != MXX && 
+                             SP_D[k][j] != MXX)
                             ? SP_D[i][k] + SP_D[k][j]
-                            : 999;
+                            : MXX;
                     if(d < SP_D[i][j]) {
                         SP_D[i][j] = d;
                         NEXT[i][j] = NEXT[i][k];
@@ -107,6 +104,9 @@ class Player
         int factoryCount = int.Parse(Console.ReadLine()); // the number of factories
         int linkCount = int.Parse(Console.ReadLine()); // the number of links between factories
         
+        
+        int bombsLeft = 2;
+        
         Dictionary<int, Cell> cells = new Dictionary<int, Cell>();
         for (int i = 0; i < linkCount; i++)
         {
@@ -121,16 +121,10 @@ class Player
             var link1 = new Link(cells[factory1], cells[factory2], dist);
             var link2 = new Link(cells[factory2], cells[factory1], dist);
             cells[factory1].Links.Add(factory2, link1);
-            cells[factory2].Links.Add(factory1, link2); 
-            
-            Console.Error.WriteLine("{0}:{1} - {2}", cells[factory1], cells[factory2], dist);
+            cells[factory2].Links.Add(factory1, link2);    
         }
         
         ComputeShortestPaths(cells);
-        Console.Error.WriteLine("Shortest Path: {0} -> {1} = {2}", cells[5], cells[6], ShortestDist(5,6));
-        foreach(var pLink in ShortestPath(5,6, cells)) {
-            Console.Error.WriteLine(" * {0}", pLink);
-        }
         Console.Error.WriteLine("-------------------------"); 
         
         // game loop
@@ -167,13 +161,29 @@ class Player
             
             var tests = new List<Tuple<int,int, int>>();
             
+            
             foreach(var myC in myCells) {
-                var interestingLinks = myC.Links.Values.Where(l => l.B.Owner != ME && l.B.Troups < myC.Troups).OrderBy(l => l.Dist).ThenBy(l => -l.B.Capacity).ThenBy(l => l.B.Troups);
+                var interestingLinks = myC.Links.Values
+                                        .Where(l => l.B.Owner != ME && l.B.Troups < myC.Troups)
+                                        .OrderBy(l => -l.B.Capacity)
+                                        .ThenBy(l => ShortestDist(myC.Id, l.B.Id))
+                                        .ThenBy(l => l.B.Troups);
                 var first = interestingLinks.FirstOrDefault();
                 Console.Error.WriteLine("{0} troups = {1}, capacity = {2} -> {3}", myC, myC.Troups, myC.Capacity, first);
                 if(first != null) {
-                    tests.Add(Tuple.Create(myC.Id, first.B.Id, first.B.Troups));
-                }
+                    var sp = ShortestPath(myC.Id, first.B.Id, cells);                    
+                    tests.Add(Tuple.Create(myC.Id, sp[0].B.Id, first.B.Troups));
+                }                            
+            }
+            
+            
+            Cell firstBombSite = null;
+            if(bombsLeft > 0) {
+                // if I have any bombs left, look through potential destinations
+                var firstChoice = othCells
+                                    .Where(c => c.Owner == CPU && c.Troups >= 10)
+                                    .OrderBy(c => -c.Capacity).ThenBy(c => -c.Troups)
+                                    .FirstOrDefault();                
             }
             
             //var myInteriorCells = myCells.Where(c => c.Links.Values.All(l => l.B.Owner == ME));
@@ -181,17 +191,31 @@ class Player
             //    Console.Error.WriteLine("Interior cell: {0} has {1} troups", ic, ic.Troups);
             //}
             
-            var move = tests.OrderBy(t => t.Item3).FirstOrDefault();
-            if(move != null) {
-                Console.WriteLine("MOVE {0} {1} {2}", move.Item1, move.Item2, move.Item3 + 1);
+            Console.Error.WriteLine(" -----  GOING FOR MOVES ----- ");
+            
+            if(tests.Count > 0) {
+                foreach(var mv in tests)
+                    Console.Write("MOVE {0} {1} {2};", mv.Item1, mv.Item2, mv.Item3 + 1);
             }else {
                 //var bestIC = myInteriorCells.OrderBy(ic => -ic.Troups).FirstOrDefault();
                 //if(bestIC != null) {       
                 //    var bestMove = bestIC.Links.Values.OrderBy(l => ShortestDist(l.A.Id, l.A.ClosestEnemy.Id));
                 //}else {                
-                    Console.WriteLine("WAIT");
+                    Console.Write("WAIT;");
                 //}
             }
+            
+            string msg = "done";
+            if(firstBombSite != null) {
+                    Console.Error.WriteLine("First choice for bombing: {0}", firstBombSite);
+                    //var origin = myCells.Where(c => !tests.Any(t => t.Item1 == c.Id))
+                     //               .FirstOrDefault();
+                    //if(
+                    
+                    msg = "" + firstBombSite.Id;
+            }
+            
+            Console.WriteLine("MSG " + msg);
         }
     }
 }
