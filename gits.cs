56159d7eff6235a78e73251c85e88ca691140e9b
Dinescu <mdinescu@amazon.com>
2017-02-26 21:49:42 -0800

    adds bombs

--- gits.cs
+++ gits.cs
@@ -10,7 +10,8 @@ class Player
     public static int ME = 1;
     public static int CPU = -1;
     public static string FACTORY = "FACTORY";
-    
+    public static string TROUP = "TROOP";
+    public static string BOMB = "BOMB";
     
     private static int[][] NEXT;
     private static int[][] SP_D;
@@ -46,6 +47,32 @@ class Player
         
         
     }
+    class Troup {
+        public Troup(int id) {
+            Id = id;
+        }
+        public int Id { get;set; }
+        public int Owner { get;set; }
+        public int From { get;set; }
+        public int To { get;set; }
+        public int Size { get;set; }
+        public int Time { get; set; }
+        
+        public override string ToString() {
+            return String.Format("T({0} from {1} to {2})", Size, From, To);
+        }
+    }
+    class Bomb {
+        public Bomb(int id) {
+            Id = id;
+        }
+        public int Id { get;set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Time { get; set; }
+        public int Owner { get; set; }
+    }
+    
     
     static void ComputeShortestPaths(Dictionary<int, Cell> cells) {
         int N = cells.Count; int MXX = int.MaxValue;
@@ -127,10 +154,24 @@ class Player
         ComputeShortestPaths(cells);
         Console.Error.WriteLine("-------------------------"); 
         
+        List<Troup> myTroups = new List<Troup>();
+        List<Troup> enemyTroups = new List<Troup>();
+        var incomingT = new Dictionary<int, List<Troup>>();
+        var outgoingT = new Dictionary<int, List<Troup>>();
+        
+        var myBombs = new List<Bomb>();
+        var enemyBombs = new List<Bomb>();
+        
+        var newBomb = false;
         // game loop
         while (true)
         {
             int entityCount = int.Parse(Console.ReadLine()); // the number of entities (e.g. factories and troops)
+            
+            myTroups.Clear(); enemyTroups.Clear();
+            incomingT.Clear(); outgoingT.Clear();
+            
+            newBomb = false; var allBombs = new List<Bomb>();
             for (int i = 0; i < entityCount; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
@@ -141,11 +182,49 @@ class Player
                 int arg3 = int.Parse(inputs[4]);
                 int arg4 = int.Parse(inputs[5]);
                 int arg5 = int.Parse(inputs[6]);                
-                Console.Error.WriteLine("E{0}.{1}: {2},{3},{4},{5}", entityType, entityId, arg1, arg2, arg3, arg4);
                 if(entityType == FACTORY) {
                     cells[entityId].Owner = arg1;
                     cells[entityId].Troups = arg2;
                     cells[entityId].Capacity = arg3;
+                    Console.Error.WriteLine("E{0}.{1}: {2},{3},{4},{5}", entityType, entityId, arg1, arg2, arg3, arg4);                    
+                }else if(entityType == TROUP) {
+                    Troup t = new Troup(entityId);
+                    t.Owner = arg1;
+                    t.From = arg2;
+                    t.To = arg3;
+                    t.Size = arg4;
+                    t.Time = arg5;
+                    
+                    if(t.Owner == ME) {
+                        myTroups.Add(t);
+                    }else {
+                        enemyTroups.Add(t);
+                    }
+                    if(!incomingT.ContainsKey(t.To)) incomingT.Add(t.To, new List<Troup>()); 
+                    incomingT[t.To].Add(t);
+                }else {                
+                    Console.Error.WriteLine("E{0}.{1}: {2},{3},{4},{5}", entityType, entityId, arg1, arg2, arg3, arg4);
+                    var bmb = new Bomb(entityId) { Owner = arg1, From = arg2, To = arg3, Time = arg4 };
+                    allBombs.Add(bmb);
+                    if (bmb.Owner == ME)
+                        myBombs.Add(bmb);
+                    else {
+                        if(!enemyBombs.Any(b => b.Id == bmb.Id)) {
+                            enemyBombs.Add(bmb);
+                            newBomb = true;
+                        }
+                    }                    
+                }
+            }
+            
+            for(int i = myBombs.Count-1; i>=0; i--) {
+                if(!allBombs.Any(b => b.Id == myBombs[i].Id)) {
+                    myBombs.RemoveAt(i);
+                }
+            }
+            for(int i = enemyBombs.Count-1; i>=0; i--) {
+                if(!allBombs.Any(b => b.Id == enemyBombs[i].Id)) {
+                    enemyBombs.RemoveAt(i);
                 }
             }
             
@@ -168,12 +247,19 @@ class Player
                                         .OrderBy(l => -l.B.Capacity)
                                         .ThenBy(l => ShortestDist(myC.Id, l.B.Id))
                                         .ThenBy(l => l.B.Troups);
-                var first = interestingLinks.FirstOrDefault();
-                Console.Error.WriteLine("{0} troups = {1}, capacity = {2} -> {3}", myC, myC.Troups, myC.Capacity, first);
-                if(first != null) {
-                    var sp = ShortestPath(myC.Id, first.B.Id, cells);                    
-                    tests.Add(Tuple.Create(myC.Id, sp[0].B.Id, first.B.Troups));
-                }                            
+                
+                int troupsLeft = myC.Troups; int reserve = myC.Capacity;
+                     
+                foreach(var il in interestingLinks) {                        
+                    Console.Error.WriteLine("{0} troups = {1}, capacity = {2} -> {3}", myC, myC.Troups, myC.Capacity, il);
+                    if(il.B.Troups + reserve < troupsLeft) {
+                        var sp = ShortestPath(myC.Id, il.B.Id, cells);                    
+                        tests.Add(Tuple.Create(myC.Id, sp[0].B.Id, il.B.Troups));
+                        troupsLeft -= (il.B.Troups+1);
+                    }
+                    if(troupsLeft <= reserve)
+                        break;
+                }                
             }
             
             
@@ -191,18 +277,60 @@ class Player
             //    Console.Error.WriteLine("Interior cell: {0} has {1} troups", ic, ic.Troups);
             //}
             
-            Console.Error.WriteLine(" -----  GOING FOR MOVES ----- ");
             
-            if(tests.Count > 0) {
-                foreach(var mv in tests)
-                    Console.Write("MOVE {0} {1} {2};", mv.Item1, mv.Item2, mv.Item3 + 1);
+            
+            if(newBomb) {
+                Console.Error.WriteLine(" -----  GOING FOR DEFENSE ----- ");
+                var defenseTarget = othCells.Where(oc => oc.Capacity >= 2)
+                                    .OrderBy(oc => -oc.Capacity)
+                                    .ThenBy(oc => oc.Troups)
+                                    //.ThenBy(oc => oc.Links[myC.Id].Dist)
+                                    .FirstOrDefault();
+                if(defenseTarget == null) {
+                    defenseTarget = othCells.Where(oc => oc.Capacity > 0)
+                                    .OrderBy(oc => -oc.Capacity)
+                                    .ThenBy(oc => oc.Troups)
+                                    //.ThenBy(oc => oc.Links[myC.Id].Dist)
+                                    .FirstOrDefault();
+                }
+                if(defenseTarget != null) {                    
+                    foreach(var myC in myCells) {
+                        if(myC.Troups > 0) {
+                            Console.Write("MOVE {0} {1} {2};", myC.Id, defenseTarget.Id, myC.Troups);
+                        }
+                    }
+                }
+                
+                if(bombsLeft > 0 && newBomb) {
+                    Console.Error.WriteLine("looing for a bomb stgy");
+                    var bomberDest = othCells.Where(oc => oc.Owner == CPU).OrderBy(oc => -oc.Capacity).FirstOrDefault();
+                    if(bomberDest != null) {
+                        Console.Error.WriteLine(".. bmb dest = " + bomberDest);
+                        var bomber = myCells
+                                    //.Where(c => c.Troups == 0)
+                                    .OrderBy(c => c.Links[bomberDest.Id].Dist)
+                                    .FirstOrDefault();
+                        if(bomber != null) {                            
+                            Console.Error.WriteLine(".. bmber = " + bomber);
+                            Console.Write("BOMB {0} {1};", bomber.Id, bomberDest.Id);
+                            bombsLeft--;                            
+                        }
+                    }
+                }
             }else {
-                //var bestIC = myInteriorCells.OrderBy(ic => -ic.Troups).FirstOrDefault();
-                //if(bestIC != null) {       
-                //    var bestMove = bestIC.Links.Values.OrderBy(l => ShortestDist(l.A.Id, l.A.ClosestEnemy.Id));
-                //}else {                
-                    Console.Write("WAIT;");
-                //}
+                Console.Error.WriteLine(" -----  GOING FOR MOVES ----- ");
+                
+                if(tests.Count > 0) {
+                    foreach(var mv in tests)
+                        Console.Write("MOVE {0} {1} {2};", mv.Item1, mv.Item2, mv.Item3 + 1);
+                }else {
+                    //var bestIC = myInteriorCells.OrderBy(ic => -ic.Troups).FirstOrDefault();
+                    //if(bestIC != null) {       
+                    //    var bestMove = bestIC.Links.Values.OrderBy(l => ShortestDist(l.A.Id, l.A.ClosestEnemy.Id));
+                    //}else {                
+                        Console.Write("WAIT;");
+                    //}
+                }
             }
             
             string msg = "done";
